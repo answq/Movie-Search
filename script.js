5b198931f3b7cb7f0157bbc5a0975bad55653a94
answq <wnffldksla@gmail.com>
2025-01-17 10:50:35 +0900

    이벤트 리스너 실행못함

--- script.js
+++ script.js
@@ -3,20 +3,19 @@
 //영화 제목 : Title
 //영화 요약 : overview
 //영화 평점 : vote_average
-const image_base_url = "image.tmdb.org/t/p/w200${movie.poster_path}";
-const movieContainer = document.querySelector("#movie-container");
-const myPoster = document.querySelector("myPoster");
-const myTitle = document.querySelector("#myTtle");
+
+const containerUL = document.querySelector("#container");
+const movieCard = document.querySelector("movie-card");
+const resultUL = document.querySelector("#result");
+const myTitle = document.querySelector("#myTitle");
 const myOverview = document.querySelector("#myOverview");
 const myReleaseDate = document.querySelector("#myReleaseDate");
-const myVoteAverage = document.querySelector("#myVoteAverage");
-const resultUL = document.querySelector("#result");
-const searchBtn = document.querySelector("#searchBtn");
-const searchInput = document.querySelector("#search");
+const myVoteRate = document.querySelector("#myVoteRate");
+const myPoster = document.querySelector("#myPoster");
+const myBookMark = document.querySelector("#myBookMark");
+let movieData = [];
 
-let postArray = [];
-
-const options = {
+let options = {
   method: "GET",
   headers: {
     accept: "application/json",
@@ -25,46 +24,99 @@ const options = {
   },
 };
 
-function fetchData() {
-  fetch(
+const fetchData = async () => {
+  const res = await fetch(
     "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
     options
-  )
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (결과) {
-      console.log("최종결과 =>", 결과);
-      postArray = 결과;
-      displayposts(결과.results);
-    });
-}
+  );
+  const 결과 = await res.json();
+  console.log("최종결과 =>", 결과);
+  postArray = 결과;
+  displayposts(결과.results);
+};
+
+fetchData(); //데이터 가져오기
+
 function displayposts(posts) {
   let emptyStr = "";
   for (let i = 0; i < posts.length; i++) {
     let movie = posts[i];
     //console.log('${i}번째 아이템 => ${posts[i]}')
 
-    emptyStr += `<li>
-<img class="movie-image" src="https://image.tmdb.org/t/p/w200${movie.poster_path}"></img> 
-   </li>
-  <li class="movie-name">${movie.title}</li>
-  <li class="movie-rate">평점 : ${movie.vote_average}
- 
-  `;
+    emptyStr += `<div class="movie-card">
+    <img class="movie-image" src="https://image.tmdb.org/t/p/w200${movie.poster_path}">
+   <div class="movie-name">${movie.title}</div>
+  <div class="movie-rate">평점 : ${movie.vote_average}</div>
+  <div class="hide">${movie.id}</div>
+  </div>`; //ul태그에 넣어줄 영화리스트 항목들
 
-    resultUL.innerHTML = emptyStr;
+    containerUL.innerHTML = emptyStr;
   }
 }
-fetchData();
 
-searchBtn.addEventListener("click", function () {
-  const keyword = searchInput.value;
+fetchData(); //데이터 리스트로 뿌려주기
 
-  const filteredPosts = postArray.filter(function (게시글하나) {
-    return 게시글하나.title.includes(keword);
+//검색창에 입력하면 필터링 된 데이터 보여주기
+const input = document.querySelector("#search");
+console.log(input);
+const searchBtn = document.querySelector("#searchBtn");
+const movieListDIV = document.querySelector("#movieList");
+console.log(movieListDIV);
+const search = function (page) {
+  const movieName = $targetMovie.value.trim();
+  if (!movieName) return;
+};
+
+const inputValue = input.value;
+const filteredData = async () => {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/search/movie?query=${inputValue}&include_adult=false&language=ko-KR&page=1",
+    options
+  );
+  const list = await res.json();
+  console.log("검색결과 =>", list);
+  const movies = list.results;
+  console.log(movies);
+
+  const filteredPosts = movies.filter(function (movie) {
+    return `<li class="search-result">
+        <img class="movie-image" src="https://image.tmdb.org/t/p/w200${movie.poster_path}">
+       <p class="movie-name">${movie.title}</p>
+      <p class="movie-rate">평점 : ${movie.vote_average}</p>
+      </li>`;
+
+    displayposts(filteredPosts);
   });
+};
 
-  console.log("fiteredPosts =>", filteredPosts);
-  displayposts(filteredPosts);
+filteredData();
+
+//영화 상세 모달창 만들기
+const modal = document.querySelector("#modal");
+const modalOpenBtn = document.querySelector(".open-btn");
+const modalCloseBtn = document.querySelector(".close-btn");
+const toggleModal = function () {
+  modal.classList.toggle("hide")
+};
+
+
+containerUL.addEventListener("click", function (event) {
+  const movieCard = event.target.closest("#");
+
+  if (movieCard) {
+    const movieId = movieCard.getAttribute("data_id");
+    const selectedMovie = movieData.find(function (movieData) {
+      return movieData.id.toString() === movieId;
+    });
+
+    if (selectedMovie) {
+      myPoster.src = `https://image.tmdb.org/t/p/w400${selectedMovie.poster_path}`;
+      myTitle.textContent = selectedMovie.title;
+      myOverview.textContent = selectedMovie.overview;
+      myReleaseDate.textContent = `개봉일 : ${selectedMovie.release_date}`;
+      myVoteRate.textContent = `평점 : ${selectedMovie.vote_average}`; //모달창에 넣어 줄 영화 상세 내용
+    }
+    toggleModal();
+  }
 });
+console.log(movieCard);
